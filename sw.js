fbf02fc114c98588ccae34ca5aa7b71addbf8998
jonathankiska-dot <jonathankiska@gmail.com>
2026-09-01 15:40:08 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    will keep serving the copy they already have. This is the single most common
    reason an update appears to do nothing. */
 
-const CACHE_VERSION = "onstrength-v13";
+const CACHE_VERSION = "onstrength-v14";
 
 const SHELL = [
   "./",
